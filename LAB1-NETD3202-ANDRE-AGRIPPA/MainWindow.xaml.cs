26a463187308ef347851ff14df1e1a94b1d5e235
home <andreagrippa76@gmail.com>
2020-10-01 21:55:47 -0400

    Add project files.

---
+++ LAB1-NETD3202-ANDRE-AGRIPPA/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LAB1_NETD3202_ANDRE_AGRIPPA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            string projectName;
+            double budget;
+            double spent;
+            double hoursRemaining;
+            string status;
+
+            if (txtProjectName.Text.Trim() != string.Empty)
+            {
+                if (double.TryParse(txtBudget.Text.Trim(), out budget))
+                {
+                    if (double.TryParse(txtSpent.Text.Trim(), out spent))
+                    {
+                        if (double.TryParse(txtHoursRemaining.Text.Trim(), out hoursRemaining))
+                        {
+                            //everything is able to parse, make sure no negative numbers
+                            if (budget >= 0)
+                            {
+                                if (spent >= 0)
+                                {
+                                    if (hoursRemaining > 0)
+                                    {
+
+                                    }
+                                    else if (hoursRemaining == 0)
+                                    {
+                                        //make cmb equal to completed
+                                        cmbStatus.SelectedIndex = 5;
+                                    }
+                                    //If hours remaining is negative
+                                    else
+                                    {
+                                        MessageBox.Show("Hours remaining must be a numeric entry greater than 0");
+                                        txtHoursRemaining.Text = "";
+                                        txtHoursRemaining.SelectAll();
+                                    }
+                                }
+                                //If spent is less than 0
+                                else
+                                {
+                                    MessageBox.Show("Spent remaining must be a numeric entry greater than 0");
+                                    txtSpent.Text = "";
+                                    txtSpent.SelectAll();
+                                }
+                            }
+                            //If budget is less than 0
+                            else
+                            {
+                                MessageBox.Show("Budget remaining must be a numeric entry greater than 0");
+                                txtBudget.Text = "";
+                                txtBudget.SelectAll();
+                            }
+                        }
+                        else
+                        {
+                            //Hours not numeric
+                            MessageBox.Show("Hours remaining cannot be a string");
+                            txtHoursRemaining.Text = "";
+                            txtHoursRemaining.SelectAll();
+                        }
+                    }
+                    else
+                    {
+                        //Spent not numeric
+                        MessageBox.Show("Spent cannot be a empty");
+                        txtSpent.Text = "";
+                        txtSpent.SelectAll();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Budget remaining cannot be empty");
+                    txtBudget.Text = "";
+                    txtBudget.SelectAll();
+                }
+            }
+            else
+            {
+                //Project string empty
+                MessageBox.Show("Project name cannot be empty");
+                txtProjectName.Text = "";
+                txtProjectName.SelectAll();
+            }
+            
+        }//End button click
+    }//End class
+}//End namespace
---
+++ LAB1-NETD3202-ANDRE-AGRIPPA/Program.cs
@@ -0,0 +1,57 @@
+﻿//These are default (keep them!)
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+//keep the default namespace too, BillingApp is just the name of my project.
+namespace LAB1_NETD3202_ANDRE_AGRIPPA
+{
+    public class Program
+    {
+        //First private data member
+        private string projectName;
+        private double budget;
+        private double spent;
+        private double hoursRemaining;
+        private string status;
+        //Complete the rest (there are four more mentioned in the description).
+        //constructor, you need to complete it this is a no parameter constructor
+        public Program()
+        {
+            
+        }
+
+        //Getters and setters for Project Name
+        public string ProjectName
+        {
+            get { return this.projectName; }
+            set { this.projectName = value; }
+
+        }
+        //Getters and setters for Budget
+        public double Budget
+        {
+            get { return this.budget; }
+            set { this.budget = value; }
+        }
+        //Getters and setters for Spent
+        public double Spent
+        {
+            get { return this.spent; }
+            set { this.spent = value; }
+        }
+        //Getters and setters for HoursRemaining
+        public double HoursRemaining
+        {
+            get { return this.HoursRemaining; }
+            set { this.HoursRemaining = value; }
+        }
+        //Getters and setters for Status
+        public double Status
+        {
+            get { return this.Status; }
+            set { this.Status = value; }
+        }
+
+    }
+}
